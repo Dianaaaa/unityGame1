0ea49f4e364492e84e0b94fdefa9726eb2192256
Dianaaaa <1035254583@qq.com>
2018-11-18 18:22:13 +0800

    18-11-18

---
+++ Game1/Assets/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject cubePrefab;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject cubeObject = Instantiate(cubePrefab);
+            cubeObject.transform.position = new Vector3(
+                Random.Range(-10f, 10f),
+                Random.Range(-10f, 10f),
+                0
+             );
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
